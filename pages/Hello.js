3b81f0c3e6e2689b05061b1bd00600a3321361c6
elamraniadnane1 <elamraniadnane1@gmail.com>
2023-02-10 17:21:01 +0100

    Test

---
+++ pages/Hello.js
@@ -0,0 +1,12 @@
+const Hello = () => {
+
+    return(
+
+        <>
+            <h1>Hello World !</h1>
+            <p>This is probably my first Next app</p>
+        </>
+    )
+}
+
+export  default Hello;
